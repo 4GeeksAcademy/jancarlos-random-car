d5fa07d55c6f1bfd418e61029be735c08c184b37
JancarlosT <138736732+JancarlosT@users.noreply.github.com>
2023-11-01 22:24:49 +0000

    card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let card = numero[Math.floor(Math.random() * numero.length)];
+  document.querySelector(".numero").innerHTML = card;
+  let palos = [
+    { simbolo: "♦", color: "red" },
+    { simbolo: "♥", color: "red" },
+    { simbolo: "♠", color: "black" },
+    { simbolo: "♣", color: "black" }
+  ];
+  let simbolo = palos[Math.floor(Math.random() * palos.length)];
+  document.querySelectorAll(".palo").forEach((element, index) => {
+    element.innerHTML = simbolo.simbolo;
+    element.style.color = simbolo.color;
+  });
   console.log("Hello Rigo from the console!");
 };
